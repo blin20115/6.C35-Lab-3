276ebc5a9d36847bffe9d2eeca512f943d6207fa
blin20115 <blin20115@gmail.com>
2026-03-10 10:40:53 -0400

    Loading data from API

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,25 @@
     import Project from "$lib/Project.svelte";
     import reading from "$lib/reading.json";
     import ReadingItem from "$lib/ReadingItem.svelte";
+    import { onMount } from "svelte";
+
+    let githubData = null; // This will eventually hold our Github stats
+    let loading = true; // This will be true *until* the fetch's promise resolves to a value
+    let error = null; // If the API call resulted in an error, it will go into this variable
+
+    onMount(async () => {
+        try {
+            let response = await fetch(
+                "https://api.github.com/users/blin20115",
+            );
+            console.log(response);
+            githubData = await response.json();
+            console.log(githubData);
+        } catch (err) {
+            error = err;
+        }
+        loading = false;
+    });
 </script>
 
 <svelte:head>
@@ -25,6 +44,26 @@
     height="450"
 />
 
+{#if loading}
+    <p>Loading...</p>
+{:else if error}
+    <p>Something went wrong: {error.message}</p>
+{:else}
+    <section>
+        <h2>My GitHub Stats</h2>
+        <dl>
+            <dt>Followers</dt>
+            <dd>{githubData.followers}</dd>
+            <dt>Following</dt>
+            <dd>{githubData.following}</dd>
+            <dt>Public Repos</dt>
+            <dd>{githubData.public_repos}</dd>
+            <dt>Public Gists</dt>
+            <dd>{githubData.public_gists}</dd>
+        </dl>
+    </section>
+{/if}
+
 <h2>Latest projects</h2>
 <div class="projects highlights">
     {#each projects.slice(0, 3) as p (p.title)}
@@ -38,3 +77,31 @@
         <ReadingItem data={{ ...item, image: `${base}${item.image}` }} />
     {/each}
 </div>
+
+<style>
+    section {
+        margin-bottom: 2rem;
+    }
+
+    dl {
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+        gap: 0.5rem;
+        text-align: center;
+    }
+
+    dt {
+        grid-row: 1;
+        font-weight: bold;
+        font-size: 0.9rem;
+        text-transform: uppercase;
+        color: gray;
+    }
+
+    dd {
+        grid-row: 2;
+        margin: 0;
+        font-size: 2rem;
+        font-weight: bold;
+    }
+</style>
